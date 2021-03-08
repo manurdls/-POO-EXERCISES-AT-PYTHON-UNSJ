6aa19bc8c0dcf50df975e174991fabbaba364601
Manuel Rossi <manurossidls@gmail.com>
2021-03-08 19:21:25 -0300

    Se agrego información de teoría U2

---
+++ Teoria POO/Unidad 2/3. En python todas las variables son objetos instancias de una clase/main.py
@@ -0,0 +1,15 @@
+def objetosPython():
+    i = 1
+    interes = 2.3
+    bandera = False
+    cadena = 'CUIL erróneo'
+    lista = [1, 2, 3]
+    print('Tipo de datos de i: {}', type(i))
+    print('Tipo de datos de interes: {}', type(interes))
+    print('Tipo de datos de bandera: {}', type(bandera))
+    print('Tipo de datos de cadena: {}', type(cadena))
+    print('Tipo de datos de lista: {}', type(lista))
+    print('Tipo de datos de objetosPython(): {}', type(objetosPython))
+
+if __name__ == '__main__':
+    objetosPython()
